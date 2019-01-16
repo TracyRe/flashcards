77e40106aa40df6a6cf302f1408bc9ae01343d41
Chris Breaux and Tracy Jo Reith <chris@>
2019-01-15 16:43:24 -0800

    add styling to terms and definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
-  $("#term1").click(function(){
-    $("#def1").removeClass("hide");
-    $("#term1").addClass("hide");
+  $("#alert-term").click(function(){
+    $("#alert-def").removeClass("hide");
+    $("#alert-term").addClass("hide");
   });
 
-  $("#def1").click(function(){
-    $("#def1").addClass("hide");
-    $("#term1").removeClass("hide");
+  $("#alert-def").click(function(){
+    $("#alert-def").addClass("hide");
+    $("#alert-term").removeClass("hide");
+  });
+
+  $("#arguments-term").click(function(){
+    $("#arguments-def").removeClass("hide");
+    $("#arguments-term").addClass("hide");
+  });
+
+  $("#arguments-def").click(function(){
+    $("#arguments-def").addClass("hide");
+    $("#arguments-term").removeClass("hide");
+  });
+
+  $("#attributes-term").click(function(){
+    $("#attributes-def").removeClass("hide");
+    $("#attributes-term").addClass("hide");
+  });
+
+  $("#attributes-def").click(function(){
+    $("#attributes-def").addClass("hide");
+    $("#attributes-term").removeClass("hide");
   });
 
   $("#showAll").click(function(){
     $(".term").addClass("hide");
     $(".definition").removeClass("hide");
-
   });
+
   $("#hideAll").click(function(){
     $(".term").removeClass("hide");
     $(".definition").addClass("hide");
